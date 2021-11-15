a58bc76ad11c70b83a5fdc7faea294aafee9c403
Anton Ovchinnikov <anton@tonyo.info>
2021-11-15 14:21:56 +0100

    Lower uwsgi values

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ if [[ "${1:-}" == "bash" ]]; then
   exec /bin/bash
 fi
 
-export UWSGI_LISTEN=${UWSGI_LISTEN:-4096}
-export UWSGI_PROCESSES=${UWSGI_PROCESSES:-16}
+export UWSGI_LISTEN=${UWSGI_LISTEN:-1024}
+export UWSGI_PROCESSES=${UWSGI_PROCESSES:-8}
 
 # Running only fake-sentry for now
 exec python -m fake_sentry.fake_sentry
