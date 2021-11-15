3d945daf563f444808b38bf2c6e8823fa2a73a3c
Anton Ovchinnikov <anton@tonyo.info>
2021-11-15 14:43:00 +0100

    Listen on all interfaces

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,7 @@ fi
 
 export UWSGI_LISTEN=${UWSGI_LISTEN:-1024}
 export UWSGI_PROCESSES=${UWSGI_PROCESSES:-8}
+export UWSGI_SOCKET=${UWSGI_SOCKET:-'0.0.0.0:8000'}
 
 # Running only fake-sentry for now
 exec python -m fake_sentry.fake_sentry
