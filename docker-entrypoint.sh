ee41b6d99f5f2203634fc80da3c0b315642496e4
Anton Ovchinnikov <anton@tonyo.info>
2021-11-15 14:09:23 +0100

    Update docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -eux
 
+if [[ "${1:-}" == "bash" ]]; then
+  exec /bin/bash
+fi
+
 export UWSGI_LISTEN=${UWSGI_LISTEN:-4096}
 export UWSGI_PROCESSES=${UWSGI_PROCESSES:-16}
 
