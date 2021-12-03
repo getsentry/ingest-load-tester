f4c84f68c41f532e131182d03112c1b843df54f0
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2021-12-03 11:32:53 +0100

    minor ( push to test repo)

--- push-image.sh
+++ push-image.sh
@@ -7,6 +7,8 @@ if [[ -n "$(git status --porcelain)" ]]; then
 fi
 
 IMAGE="us.gcr.io/sentryio/ingest-load-tester"
+# uncomment next line to push to sentry testing repo
+# IMAGE="europe-west3-docker.pkg.dev/sentry-st-testing/main/ingest-load-tester"
 TAG=$(git rev-parse HEAD)
 
 docker build -t $IMAGE:$TAG .
