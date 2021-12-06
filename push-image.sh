2ae7c71207e8ce3355646217139e00a1ca9d23dd
Anton Ovchinnikov <anton@tonyo.info>
2021-12-06 13:48:37 +0100

    ref(build): Switch to artifact registry in sentry-st-testing (#24)

--- push-image.sh
+++ push-image.sh
@@ -6,11 +6,9 @@ if [[ -n "$(git status --porcelain)" ]]; then
   exit 1
 fi
 
-IMAGE="us.gcr.io/sentryio/ingest-load-tester"
-# uncomment next line to push to sentry testing repo
-# IMAGE="europe-west3-docker.pkg.dev/sentry-st-testing/main/ingest-load-tester"
+IMAGE="europe-west3-docker.pkg.dev/sentry-st-testing/main/ingest-load-tester"
 TAG=$(git rev-parse HEAD)
 
-docker build -t $IMAGE:$TAG .
+docker build -t "${IMAGE}:${TAG}" .
 
-docker push $IMAGE:$TAG
+docker push "${IMAGE}:${TAG}"
