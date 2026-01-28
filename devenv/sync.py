60e88d052867d875bc4d8449a4c265c965cf1754
Mark Story <mark@mark-story.com>
2026-01-28 14:57:52 -0500

    Simplify devenv

--- devenv/sync.py
+++ devenv/sync.py
@@ -5,7 +5,7 @@ import importlib
 from shutil import which
 
 from devenv.constants import SYSTEM_MACHINE, homebrew_bin, home, root
-from devenv.lib import brew, config, gcloud, proc, tenv, uv, venv
+from devenv.lib import brew, config, gcloud, proc, uv, venv
 
 def install_gcloud_kubectl(gcloud: str) -> None:
     if which("kubectl") is None:
@@ -95,21 +95,6 @@ def main(context: dict[str, str]) -> int:
 
     brew.install()
 
-    proc.run(
-        (f"{homebrew_bin}/brew", "bundle"),
-        cwd=reporoot,
-    )
-
-    try:
-        proc.run(
-            ("helm", "plugin", "install", "https://github.com/databus23/helm-diff", "--verify=false"),
-            cwd=reporoot,
-            stdout=True
-        )
-    except RuntimeError as e:
-        if "Error: plugin already exists" not in str(e): # Sad workaround since there's no way to ignore this in helm
-            raise SystemExit(e)
-
     gcloud.install(
         cfg["gcloud"]["version"],
         cfg["gcloud"][SYSTEM_MACHINE],
@@ -117,36 +102,6 @@ def main(context: dict[str, str]) -> int:
         reporoot,
     )
 
-    install_gcloud_kubectl(f"{reporoot}/.devenv/bin/gcloud")
-
     ensure_gcloud_authed(f"{reporoot}/.devenv/bin/gcloud")
-
-    tenv.install(
-        cfg["tenv"]["version"],
-        cfg["tenv"][SYSTEM_MACHINE],
-        cfg["tenv"][f"{SYSTEM_MACHINE}_sha256"],
-        reporoot,
-    )
-
-    for name in ("sentry-kube", "salt"):
-        venv_dir, python_version, requirements, editable_paths, bins = venv.get(reporoot, name)
-        url, sha256 = config.get_python(reporoot, python_version)
-        print(f"ensuring {name} venv at {venv_dir}...")
-        venv.ensure(venv_dir, python_version, url, sha256)
-
-        print(f"syncing {name} with {requirements}...")
-        venv.sync(reporoot, venv_dir, requirements, editable_paths, bins)
-
-    need_cluster_credentials = True
-
-    if os.path.exists(f"{home}/.kube/config"):
-        with open(f"{home}/.kube/config", "r") as f:
-            for line in f:
-                if "zdpwkxst" in line:
-                    need_cluster_credentials = False
-                    break
-
-    if need_cluster_credentials:
-        proc.run(("bin/gke-credentials-setup",), cwd=reporoot)
 
     return 0
