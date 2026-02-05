6a9f54a1843f70eb86af514e7b78ea8916094f9c
Mark Story <mark@mark-story.com>
2026-02-05 15:34:33 -0800

    Add logs User factory.

--- s4s2_locustfile.py
+++ s4s2_locustfile.py
@@ -18,10 +18,12 @@ file_event_task_factory = event_tasks.file_event_task_factory
 file_envelope_event_task_factory = event_tasks.file_envelope_event_task_factory
 session_event_task_factory = event_tasks.session_event_task_factory
 transaction_event_task_factory = event_tasks.transaction_event_task_factory
+log_envelope_task_factory = event_tasks.log_envelope_task_factory
 
 # Load config and add config to factories.
 _config_path = full_path_from_module_relative_path(__file__, "config/s4s2.test.yml")
 
-# SimpleLoadTest = create_user_class("SimpleLoadTest", _config_path, __name__)
-RandomEvents = create_user_class("RandomEvents", _config_path, __name__)
 # TransactionEvents = create_user_class("TransactionEvents", _config_path, __name__)
+# SimpleLoadTest = create_user_class("SimpleLoadTest", _config_path, __name__)
+# RandomEvents = create_user_class("RandomEvents", _config_path, __name__)
+LogEvents = create_user_class("LogEvents", _config_path, __name__)
